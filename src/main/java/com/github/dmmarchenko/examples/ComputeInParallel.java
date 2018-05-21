1013c755706e655c46ea06601e41096cd265d5c9
Dmytro Marchenko <dmmarchenko@corelogic.com>
2018-05-21 15:46:14 +0300

    Added ComputeInParallel example

---
+++ src/main/java/com/github/dmmarchenko/examples/ComputeInParallel.java
@@ -0,0 +1,43 @@
+package com.github.dmmarchenko.examples;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.util.function.Tuple4;
+
+@Slf4j
+public class ComputeInParallel {
+
+    public static void main(String[] args) {
+        Tuple4<Integer, Integer, Optional<Integer>, Integer> result = Mono.zip(
+            call(() -> longTask(1)),
+            call(() -> longTask(2)),
+            call(ComputeInParallel::nullTask).defaultIfEmpty(Optional.empty()),
+            call(() -> longTask(3)))
+            .block();
+
+        log.info("Result: {}", result);
+    }
+
+    private static <T> Mono<T> call(final Supplier<T> supplier) {
+        return Mono.fromSupplier(supplier)
+            .subscribeOn(Schedulers.parallel());
+    }
+
+    private static Optional<Integer> nullTask() {
+        return null;
+    }
+
+    @SneakyThrows
+    private static Integer longTask(int id) {
+        log.info("Starting task: {}", id);
+        TimeUnit.SECONDS.sleep(5);
+        log.info("Ending task: {}", id);
+        return id * 2;
+    }
+}
