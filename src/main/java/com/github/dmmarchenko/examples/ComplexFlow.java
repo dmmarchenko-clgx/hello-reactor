883926023de479eac91f20bd6861d3760d146a45
Dmytro Marchenko <dmmarchenko@corelogic.com>
2018-05-23 11:10:04 +0300

    analog to coordination example

--- src/main/java/com/github/dmmarchenko/examples/ComplexFlow.java
+++ src/main/java/com/github/dmmarchenko/examples/ComplexFlow.java
@@ -1,35 +1,70 @@
 package com.github.dmmarchenko.examples;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
+import com.google.common.collect.ImmutableList;
+
+import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @Slf4j
 public class ComplexFlow {
 
     public static void main(String[] args) {
-        Context context = new Context();
+        ImmutableList<Mono<ResponseBuilder>> monos = ImmutableList.of(
+            createTask("Marchenko", 3).map(ResponseBuilderImpl::new),
+            createTask("Dmytro", 3).map(ResponseBuilderImpl::new),
+            createTask("Viktorovych", 3).map(ResponseBuilderImpl::new)
+        );
+
+        ResponseBuilder builder = Flux.fromIterable(monos)
+            .flatMap(Function.identity())
+            .reduce((x, y) -> x.andThen(y))
+            .block();
+
+        String fullName = builder.apply(new StringBuilder()).toString();
+
+        log.info("Result: {}", fullName);
     }
 
     @SneakyThrows
-    private static <T> T longMap(T object, int duration) {
-        TimeUnit.SECONDS.sleep(duration);
-        return object;
+    private static void waitSeconds(int seconds) {
+        TimeUnit.SECONDS.sleep(seconds);
     }
 
-    private static class Context {
+    private static Mono<String> createTask(String value, int duration) {
+        return Mono.fromSupplier(() -> {
+            waitSeconds(duration);
+            log.info("Completed task: {}", value);
+            return value;
+        });
+    }
 
-        private final Map<String, String> values = new HashMap<>();
+    @AllArgsConstructor
+    private static class ResponseBuilderImpl implements ComplexFlow.ResponseBuilder {
 
-        private void put(String key, String value) {
-            values.put(key, value);
+        private String value;
+
+        @Override
+        public StringBuilder apply(final StringBuilder stringBuilder) {
+            return stringBuilder
+                .append(value)
+                .append(" ");
         }
+    }
 
-        private String get(String key) {
-            return values.get(key);
+    private interface ResponseBuilder extends UnaryOperator<StringBuilder> {
+
+        default ResponseBuilder andThen(ResponseBuilder after) {
+            Objects.requireNonNull(after);
+            return (StringBuilder response) -> after.apply(apply(response));
         }
     }
 }
